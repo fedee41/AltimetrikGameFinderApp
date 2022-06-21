b5be719020e2aefabac3f39564096bf3cb4a4869
fedee41 <federicomelgarejo2@gmail.com>
2022-06-21 18:42:13 -0300

    develop1.2

--- GameFinder/app/jsgamefinder.js
+++ GameFinder/app/jsgamefinder.js
@@ -1,23 +1,22 @@
 
-name = []:
-backfroundimg = [];
+name = [];
+backgroundimg = [];
 
 
 fetch('https://api.rawg.io/api/games?key=d26560450ced42a9be32fcb6d8b1103d')
 .then(response => response.json())
-.then(json => {
-  console.log(json)
+.then(data => {
+  console.log(data.results.length)
   let i=0;
-  while (i<json.length) {
-    name = json.results[i].name;
-    backfroundimg = json.results[i].background_image;
-    console.log( name)
+  while (i<data.results.length) {
+    name[i] = data.results[i].name;
+    backgroundimg[i] = data.results[i].background_image;
   }
 
     
     }
 )
 
-function laoddata(){
+function loaddata(){
 
 }

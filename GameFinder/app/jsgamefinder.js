d0b5b2fcd24bc1a8c3ff0f033a44dc1120ec7c54
unknown <federicomelgarejo2@gmail.com>
2022-06-24 00:14:19 -0300

    develop1.5

--- GameFinder/app/jsgamefinder.js
+++ GameFinder/app/jsgamefinder.js
@@ -1,22 +1,16 @@
+const url = "https://api.rawg.io/api/games?key=d26560450ced42a9be32fcb6d8b1103d";
 
-name = [];
-backgroundimg = [];
+var requestOptions = {
+  method: "GET",
+  headers: new Headers({ "Content-Type": "application/json" }),
+  redirect: "follow",
+};
 
+fetch(url, requestOptions)
+  .then(response => response.json())
+  .then((result) => getGames(result))
+  .catch((error) => console.log("error", error));
 
-fetch('https://api.rawg.io/api/games?key=d26560450ced42a9be32fcb6d8b1103d')
-.then(response => response.json())
-.then(data => {
-  console.log(data.results.length)
-  let i=0;
-  while (i<data.results.length) {
-    name[i] = data.results[i].name;
-    backgroundimg[i] = data.results[i].background_image;
-  }
-
-    
-    }
-)
-
-function loaddata(){
-
-}
+  function getGames(result) {
+    console.log(result)
+  };
--- GameFinder/login/jsdblogin.js
+++ GameFinder/login/jsdblogin.js
@@ -1,48 +1,60 @@
+function handleSubmit(e) {
+  e.preventDefault()
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data.user))
+    .catch ((err) => console.error('Request Failed', (err))) /*Catch errors*/
+}
+
+
+
 //data of email and password from html
-emailHTML = document.getElementById('email');
-passwordHTML = document.getElementById('password');
+// emailHTML = document.getElementById('email');
+// passwordHTML = document.getElementById('password');
 //array to obtain data
-emailJSON = [];
-id = [];
-passwordJSON = [];
-flag = false;       /*the flag give me if i have access to login */
+// emailJSON = [];
+// id = [];
+// passwordJSON = [];
+// flag = false;       /*the flag give me if i have access to login */
 
 
-fetch('http://localhost:3000/users')
-.then(response => response.json())
-.then(data => {
-  console.log(data)
-  let i=0;
-    while (i <data.length){
-        id[i] = data[i].id;
-        emailJSON[i] = data[i].email;
-        passwordJSON[i] =data[i].password; 
-        console.log("id:" + data[i].id + "email:" + data[i].email + "password:" + data[i].password);
-        ++i;
-    }
-})
-  .catch ((err) => console.error('Request Failed', (err))), /*Catch errors*/
+// fetch('http://localhost:3000/users')
+// .then(response => response.json())
+// .then(data => {
+  // console.log(data)
+  // let i=0;
+    // while (i <data.length){
+        // id[i] = data[i].id;
+        // emailJSON[i] = data[i].email;
+        // passwordJSON[i] =data[i].password; 
+        // console.log("id:" + data[i].id + "email:" + data[i].email + "password:" + data[i].password);
+        // ++i;
+    // }
+// })
+  // .catch ((err) => console.error('Request Failed', (err))), /*Catch errors*/
 
   //funcionality of Login button
-  document.getElementById("login").addEventListener("click",() => {
-    let i = 0;
-    console.log("Validation" + "email: " + emailHTML.value + " password: " + passwordHTML.value);
-    //validation
-    while (i<emailJSON.length){
-        if(emailHTML.value == emailJSON[i] && passwordHTML.value == passwordJSON[i]) {
-            console.log("Now you are logged in");
-            flag = true;
-        }
-        console.log("id:" + id[i] + "email:" + emailJSON[i] + "password:" + passwordJSON[i] + "\n")
-        ++i;
-    }
-    if(flag === true){
-      window.open('../app/gamefinder.html');
-      window.close();
-    }
-    else{
-      console.log("Incorrect password")
-    }
-  })
-
-  
+  // document.getElementById("login").addEventListener("click",() => {
+    // let i = 0;
+    // console.log("Validation" + "email: " + emailHTML.value + " password: " + passwordHTML.value);
+    // //validation
+    // while (i<emailJSON.length){
+        // if(emailHTML.value == emailJSON[i] && passwordHTML.value == passwordJSON[i]) {
+            // console.log("Now you are logged in");
+            // flag = true;
+        // // }
+        // console.log("id:" + id[i] + "email:" + emailJSON[i] + "password:" + passwordJSON[i] + "\n")
+        // ++i;
+    // }
+    // if(flag === true){
+      // window.open('../app/gamefinder.html');
+      // window.close();
+    // }
+    // else{
+      // console.log("Incorrect password")
+    // }
+  // })
